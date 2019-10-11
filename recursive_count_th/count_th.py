301b511be0c72a39dca8bfa61cd172555416f61d
basilcea <ogbonnabasil3@gmail.com>
2019-10-11 11:10:32 +0100

    count_th

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,26 +5,16 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
     # first split the word
-    array = list(word)
-    print(array)
     # initialise number of movements across the array and the count
-    number = 0
-    count = 0
     # set base case to when it has moved through the whole array to stop
-
-    if number == len(array):
-        return count
-    if(array[number] == 't' and  array[number + 1] == 'h'):
-       count +=1
-    number +=1
-    print(number)
-
-    count_th(array[number : len(array)+1])
-
-    
+    if word == '':
+        return 0
+    if word[:2] == 'th':
+        return 1 + count_th(word[1:])
+    else:
+        return 0 + count_th(word[1:])
 
     #then  get the index of the first t
 
     # TBC
     
-print(count_th('abcthxyz'))
